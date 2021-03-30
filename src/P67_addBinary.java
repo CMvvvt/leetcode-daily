f588023faa2d5f1ed8166c2cd4d212f78a2026b2
CMvvvt <452000761@qq.com>
2021-03-30 14:30:57 +0800

    p67 finished

---
+++ src/P67_addBinary.java
@@ -0,0 +1,20 @@
+public class P67_addBinary {
+    class Solution {
+        public String addBinary(String a, String b) {
+            StringBuffer ans = new StringBuffer();
+            int n = Math.max(a.length(), b.length());
+            int carry = 0;
+            for(int i = 0; i < n; i++) {
+                carry += i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
+                carry += i < b.length() ? (b.charAt(b.length() - 1 -i) - '0') : 0;
+                ans.append((char) (carry % 2 + '0'));
+                carry /= 2;
+            }
+            if(carry > 0) {
+                ans.append('1');
+            }
+            ans.reverse();
+            return ans.toString();
+        }
+    }
+}
