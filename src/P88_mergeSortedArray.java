780ca5c78265a171426fce4240e009c67f825a75
CMvvvt <452000761@qq.com>
2021-04-06 14:05:31 +0800

    p88 method added

--- src/P88_mergeSortedArray.java
+++ src/P88_mergeSortedArray.java
@@ -19,4 +19,23 @@ public class P88_mergeSortedArray {
             }
         }
     }
+
+//法二 提前结束，当第二个数组全部放入数组一时，提前结束
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int p1 = m - 1;
+            int p2 = n - 1;
+            int tail = m + n - 1;
+            while(p1 >= 0 || p2 >= 0) {
+                if(p2 == -1) {
+                    return;
+                } else if(p1 == -1) {
+                    nums1[tail--] = nums2[p2--];
+                } else if (nums1[p1] > nums2[p2]) {
+                    nums1[tail--] = nums1[p1--];
+                } else {
+                    nums1[tail--] = nums2[p2--];
+                }
+            }
+        }
 }
+
