2704546d6bd9368d5624d06cc01373aca4112f02
CMvvvt <452000761@qq.com>
2021-04-07 16:35:13 +0800

    p81 finished

---
+++ src/P81_searchRotatedSortedArray.java
@@ -0,0 +1,30 @@
+public class P81_searchRotatedSortedArray class Solution {
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        if(n == 0) return false;
+        if(n == 1) return nums[0] == target;
+        int l = 0, r = n - 1;
+        while(l <= r) {
+            int mid = l + (r-l) / 2;
+            if(nums[mid] == target) return true;
+            if(nums[l] == nums[mid] && nums[mid] == nums[r]) {
+                l++;
+                r--;
+            } else if(nums[l] <= nums[mid]) {
+                if(target >= nums[l] && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if(target > nums[mid] && target <= nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
+}
