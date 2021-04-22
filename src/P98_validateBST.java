aff8ee38bfa1eb6f96394ad025a0abbe78780fb9
CMvvvt <452000761@qq.com>
2021-04-22 22:01:47 +0800

    P98 method added

--- src/P98_validateBST.java
+++ src/P98_validateBST.java
@@ -34,4 +34,15 @@ public class P98_validateBST {
             return;
         }
     }
+
+    class Solution2 {
+        public boolean isValidBST(TreeNode root) {
+            return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        }
+        private boolean helper(TreeNode root, long low, long high) {
+            if(root == null) return true;
+            if(root.val <= low || root.val >= high) return false;
+            return helper(root.left, low, root.val) && helper(root.right, root.val, high);
+        }
+    }
 }
