5ba36a2883b1099552ce86afba599aaa1ec78ef6
CMvvvt <452000761@qq.com>
2021-04-22 23:25:22 +0800

    P99 new method added

--- src/P99_recoverBST.java
+++ src/P99_recoverBST.java
@@ -57,5 +57,49 @@ public class P99_recoverBST {
             }
         }
     }
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution2 {
+        public void recoverTree(TreeNode root) {
+            TreeNode x = null, y = null, pre = null;
+            Deque<TreeNode> stack = new ArrayDeque<>();
+            while(!stack.isEmpty() || root != null) {
+                while(root != null) {
+                    stack.push(root);
+                    root = root.left;
+                }
+                root = stack.pop();
+                if(pre != null && root.val < pre.val) {
+                    y = root;
+                    if(x == null) {
+                        x = pre;
+                    } else {
+                        break;
+                    }
+                }
+                pre = root;
+                root = root.right;
+            }
 
+            swap(x, y);
+        }
+        private void swap(TreeNode x, TreeNode y) {
+            int temp = x.val;
+            x.val = y.val;
+            y.val = temp;
+        }
+    }
 }
