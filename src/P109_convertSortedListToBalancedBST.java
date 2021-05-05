5003fbaa18f482c1ce785711f4182022faebe065
CMvvvt <452000761@qq.com>
2021-05-05 20:46:27 +0800

    p109 adde

--- src/P109_convertSortedListToBalancedBST.java
+++ src/P109_convertSortedListToBalancedBST.java
@@ -50,4 +50,24 @@ public class P109_convertSortedListToBalancedBST {
             return root;
         }
     }
+
+    class Solution2 {
+        public TreeNode sortedListToBST(ListNode head) {
+            if(head == null) return null;
+            return helper(head, null);
+        }
+        private TreeNode helper(ListNode head, ListNode tail) {
+            if(head == tail) return null;
+            ListNode fast = head;
+            ListNode slow = head;
+            while(fast != tail && fast.next != tail) {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            TreeNode root = new TreeNode(slow.val);
+            root.left = helper(head, slow);
+            root.right = helper(slow.next, tail);
+            return root;
+        }
+    }
 }
