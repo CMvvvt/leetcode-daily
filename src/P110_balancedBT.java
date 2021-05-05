4f8f3237a6c972c057b735e8cb46ec5fcf05836f
CMvvvt <452000761@qq.com>
2021-05-05 21:40:52 +0800

    p110 new solution added

--- src/P110_balancedBT.java
+++ src/P110_balancedBT.java
@@ -27,4 +27,35 @@ public class P110_balancedBT {
             return Math.max(getLevel(node.left), getLevel(node.right)) + 1;
         }
     }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution2 {
+        public boolean isBalanced(TreeNode root) {
+            return height(root) >= 0;
+        }
+        private int height(TreeNode root) {
+            if(root == null) return 0;
+            int leftTree = height(root.left);
+            int rightTree = height(root.right);
+            if(leftTree == -1 || rightTree == -1 || Math.abs(leftTree-rightTree) > 1) {
+                return -1;
+            } else {
+                return Math.max(leftTree, rightTree) + 1;
+            }
+        }
+    }
 }
