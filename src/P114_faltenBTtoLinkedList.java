69b93c24b15ff096ff805f7a2b071cc66ea0e014
CMvvvt <452000761@qq.com>
2021-05-06 18:01:50 +0800

    p114 new method added

--- src/P114_faltenBTtoLinkedList.java
+++ src/P114_faltenBTtoLinkedList.java
@@ -36,4 +36,39 @@ public class P114_faltenBTtoLinkedList {
             return;
         }
     }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution2 {
+        public void flatten(TreeNode root) {
+            TreeNode curr = root;
+            while(curr != null) {
+                if(curr.left != null) {
+                    TreeNode next = curr.left;
+                    TreeNode predecessor = next;
+                    while(predecessor.right != null) {
+                        predecessor = predecessor.right;
+                    }
+                    predecessor.right = curr.right;
+                    curr.left = null;
+                    curr.right = next;
+                }
+                curr = curr.right;
+            }
+            return;
+        }
+    }
 }
