bd1649f6894d72b6a8ec5fd9ce43573a5a78fcc6
CMvvvt <452000761@qq.com>
2021-05-06 11:09:05 +0800

    p111 added

--- src/P111_minimumDepthBT.java
+++ src/P111_minimumDepthBT.java
@@ -27,4 +27,12 @@ class Solution {
         return minDepth+1;
     }
 }
+    class Solution2 {
+        public int minDepth(TreeNode root) {
+            if(root == null) return 0;
+            int left = minDepth(root.left);
+            int right = minDepth(root.right);
+            return (root.left == null || root.right == null) ? left + right + 1: Math.min(left, right) + 1;
+        }
+    }
 }
