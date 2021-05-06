33f18bee0070c5087ad2a13a92e9659529c28e55
CMvvvt <452000761@qq.com>
2021-05-06 12:09:41 +0800

    p112 method dded

--- src/P112_pathSum.java
+++ src/P112_pathSum.java
@@ -22,4 +22,36 @@ public class P112_pathSum {
             return (hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum-root.val));
         }
     }
+
+    /**
+     * 迭代法 queue
+     */
+    class Solution2 {
+        public boolean hasPathSum(TreeNode root, int targetSum) {
+            if(root == null) return false;
+            Queue<TreeNode> node = new LinkedList<>();
+            Queue<Integer> val = new LinkedList<>();
+            node.offer(root);
+            val.offer(root.val);
+            while(!node.isEmpty()) {
+                TreeNode tempNode = node.poll();
+                int tempVal = val.poll();
+                if(tempNode.left == null && tempNode.right == null) {
+                    if(tempVal == targetSum) {
+                        return true;
+                    }
+                    continue;
+                }
+                if(tempNode.left != null) {
+                    node.offer(tempNode.left);
+                    val.offer(tempVal + tempNode.left.val);
+                }
+                if(tempNode.right != null) {
+                    node.offer(tempNode.right);
+                    val.offer(tempVal + tempNode.right.val);
+                }
+            }
+            return false;
+        }
+    }
 }
