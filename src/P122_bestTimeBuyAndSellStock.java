00517f5633ca70ae568517bf4e215d21a748800b
CMvvvt <452000761@qq.com>
2021-05-08 12:58:31 +0800

    p122 new method added

--- src/P122_bestTimeBuyAndSellStock.java
+++ src/P122_bestTimeBuyAndSellStock.java
@@ -27,4 +27,17 @@ public class P122_bestTimeBuyAndSellStock {
             return Math.max(dp0, dp1);
         }
     }
+
+    class Solution3 {
+        public int maxProfit(int[] prices) {
+            if(prices == null || prices.length <= 1) return 0;
+            int ans = 0;
+            for(int i = 1; i < prices.length; i++) {
+                if(prices[i] > prices[i-1]) {
+                    ans += (prices[i] - prices[i-1]);
+                }
+            }
+            return ans;
+        }
+    }
 }
