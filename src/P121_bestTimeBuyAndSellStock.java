3bd3a56d3b0b507eb89b862d966114877bc785b4
CMvvvt <452000761@qq.com>
2021-05-08 11:37:26 +0800

    P121 added

---
+++ src/P121_bestTimeBuyAndSellStock.java
@@ -0,0 +1,16 @@
+public class P121_bestTimeBuyAndSellStock {
+    class Solution {
+        public int maxProfit(int[] prices) {
+            int minPrice = Integer.MAX_VALUE;
+            int maxGross = 0;
+            for(int i = 0; i < prices.length; i++) {
+                if(prices[i] < minPrice) {
+                    minPrice = prices[i];
+                } else if(prices[i] - minPrice > maxGross) {
+                    maxGross = prices[i] - minPrice;
+                }
+            }
+            return maxGross;
+        }
+    }
+}
