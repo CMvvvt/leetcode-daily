265aef89e645ce6155a696dd90ab3e9a34224e38
CMvvvt <452000761@qq.com>
2021-05-14 13:35:28 +0800

    P12 2nd time

--- src/P12_intToRoman.java
+++ src/P12_intToRoman.java
@@ -101,4 +101,25 @@ public class P12_intToRoman {
         assertEquals("MMCCCXLIX",intTorRoman_method2(test));
 
     }
+
+    class Solution2 {
+        private int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        private String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        public String intToRoman(int num) {
+            StringBuilder res = new StringBuilder();
+            for(int i = 0; i < values.length; i++){
+                int val = values[i];
+                String s = symbols[i];
+                while(num >= val) {
+                    num -= val;
+                    res.append(s);
+                }
+                if(num == 0) {
+                    break;
+                }
+            }
+            return res.toString();
+        }
+
+    }
 }
