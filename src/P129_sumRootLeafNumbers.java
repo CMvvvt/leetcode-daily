446d7ad39c102e950ddc6d0bdb4791def141ce8d
CMvvvt <452000761@qq.com>
2021-05-21 21:12:38 +0800

    p129 method added

--- src/P129_sumRootLeafNumbers.java
+++ src/P129_sumRootLeafNumbers.java
@@ -38,4 +38,17 @@ public class P129_sumRootLeafNumbers {
             return;
         }
     }
+    class Solution2 {
+        public int sumNumbers(TreeNode root) {
+            return dfs(root, 0);
+        }
+        private int dfs(TreeNode root, int preSum) {
+            if(root == null) return 0;
+            int sum = preSum * 10 + root.val;
+            if(root.left == null && root.right == null) {
+                return sum;
+            }
+            return dfs(root.left, sum) + dfs(root.right, sum);
+        }
+    }
 }
