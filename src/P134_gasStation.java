333cfb2dc060a9c085e4140c7cf44090a6bc31c9
CMvvvt <452000761@qq.com>
2021-05-22 18:51:53 +0800

    P134 new method added

--- src/P134_gasStation.java
+++ src/P134_gasStation.java
@@ -41,4 +41,29 @@ public class P134_gasStation {
             return true;
         }
     }
+    class Solution2 {
+        public int canCompleteCircuit(int[] gas, int[] cost) {
+            int n  = gas.length;
+            int i = 0;
+            while(i < n) {
+                int sumGas = 0, sumCost = 0;
+                int cnt = 0;
+                while(cnt < n) {
+                    int j = (i + cnt) % n;
+                    sumGas += gas[j];
+                    sumCost += cost[j];
+                    if(sumCost > sumGas) {
+                        break;
+                    }
+                    cnt++;
+                }
+                if(cnt == n) {
+                    return i;
+                } else {
+                    i = i + cnt + 1;
+                }
+            }
+            return -1;
+        }
+    }
 }
