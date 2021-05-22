49fa05ace6d902ced6521c4b334e4308142e2722
CMvvvt <452000761@qq.com>
2021-05-22 20:31:49 +0800

    P138 new method added

--- src/P138_copyListWithRandomPointer.java
+++ src/P138_copyListWithRandomPointer.java
@@ -59,4 +59,39 @@ class Node {
 
         }
     }
+
+
+    /*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+    class Solution2 {
+        Map<Node, Node> visitedMap = new HashMap<>();
+        public Node copyRandomList(Node head) {
+            if(head == null) return null;
+            if(this.visitedMap.containsKey(head)) {
+                return this.visitedMap.get(head);
+            }
+
+            Node node = new Node(head.val, null, null);
+
+            this.visitedMap.put(head, node);
+
+            node.next = copyRandomList(head.next);
+            node.random = copyRandomList(head.random);
+
+            return node;
+        }
+    }
 }
