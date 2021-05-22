d8809dd03a0a35c787d26358e46d42ad079a68c7
CMvvvt <452000761@qq.com>
2021-05-22 19:58:49 +0800

    P138 added

---
+++ src/P138_copyListWithRandomPointer.java
@@ -0,0 +1,62 @@
+public class P138_copyListWithRandomPointer {
+    /*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+    class Solution {
+        public Node copyRandomList(Node head) {
+            Node root = new Node(0);
+            Node dummy = root;
+            Node root2 = root;
+            Node dummyHead = head;
+            Node dummyHead2 = head;
+            if(head == null) return null;
+            int len = 0;
+            while(head != null) {
+                len++;
+                head = head.next;
+            }
+            int[] arr = new int[len];
+            Map<Integer, Node> map = new HashMap<>();
+            for(int i = 0; i < len; i++) {
+                root.val = i;
+                if(i == len - 1) {
+                    root.next = null;
+                } else {
+                    root.next = new Node(i);
+                }
+                map.put(i, root);
+                arr[i] = dummyHead2.val;
+                dummyHead2.val = i;
+                root = root.next;
+                dummyHead2 = dummyHead2.next;
+            }
+
+            for(int i = 0; i < len; i++) {
+                if(dummyHead.random == null) {
+                    root2.random = null;
+                    root2.val = arr[i];
+                } else {
+                    root2.random = map.get(dummyHead.random.val);
+                    root2.val = arr[i];
+                }
+                root2 = root2.next;
+                dummyHead = dummyHead.next;
+            }
+
+            return dummy;
+
+        }
+    }
+}
