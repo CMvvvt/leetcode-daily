d610997ccb9c1eeb1da32562f9d8412f0b5326e2
CMvvvt <452000761@qq.com>
2021-06-02 15:42:17 +0800

    p523 new method added

--- src/P523_continuousSubarraySum.java
+++ src/P523_continuousSubarraySum.java
@@ -20,4 +20,26 @@ public class P523_continuousSubarraySum {
             return false;
         }
     }
+
+    class Solution2 {
+        public boolean checkSubarraySum(int[] nums, int k) {
+            int length = nums.length;
+            if(length < 2) return false;
+            int[] sum = new int[length+1];
+            sum[0] = nums[0];
+
+            for(int i = 1; i <= length; i++) {
+                sum[i] = sum[i-1] + nums[i-1];
+            }
+            Set<Integer> set = new HashSet<>();
+            for(int i = 2; i <= length; i++) {
+                set.add(sum[i-2] % k);
+                if(set.contains(sum[i] % k)) {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+    }
 }
