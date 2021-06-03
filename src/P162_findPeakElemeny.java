5010bc0144bb45e66f2932c8def96a492f89960d
CMvvvt <452000761@qq.com>
2021-06-03 19:35:03 +0800

    P162 new method added

--- src/P162_findPeakElemeny.java
+++ src/P162_findPeakElemeny.java
@@ -30,4 +30,17 @@ public class P162_findPeakElemeny {
             return head1;
         }
     }
+
+    class Solution2 {
+        public int findPeakElement(int[] nums) {
+            int n = nums.length;
+            return helper(nums, 0, n-1);
+        }
+        private int helper(int[] nums, int l, int r) {
+            if(l == r) return l;
+            int mid = (l+r)/2;
+            if(nums[mid] > nums[mid+1]) return helper(nums, l, mid);
+            return helper(nums, mid+1, r);
+        }
+    }
 }
