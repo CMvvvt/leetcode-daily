480c3f7c7041642c99f0807c3bb704c6b93c4115
CMvvvt <452000761@qq.com>
2021-06-05 20:10:49 +0800

    P169 adde

---
+++ src/P169_majorityElement.java
@@ -0,0 +1,15 @@
+public class P169_majorityElement {
+    class Solution {
+        public int majorityElement(int[] nums) {
+            int n = nums.length;
+            Map<Integer,Integer> majorityMap = new HashMap<>();
+            for(int i = 0; i < n; i++) {
+                majorityMap.put(nums[i], majorityMap.getOrDefault(nums[i], 0) + 1);
+                if(majorityMap.get(nums[i]) > n/2) {
+                    return nums[i];
+                }
+            }
+            return -1;
+        }
+    }
+}
