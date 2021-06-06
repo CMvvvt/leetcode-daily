e44e3ac5ccabe75cfa393499524964dc3af74250
CMvvvt <452000761@qq.com>
2021-06-06 10:54:55 +0800

    P172 added

---
+++ src/P172_factorialTrailingZeroes.java
@@ -0,0 +1,12 @@
+public class P172_factorialTrailingZeroes {
+    class Solution {
+        public int trailingZeroes(int n) {
+            int count = 0;
+            while(n > 0) {
+                count = count + n / 5;
+                n /= 5;
+            }
+            return count;
+        }
+    }
+}
