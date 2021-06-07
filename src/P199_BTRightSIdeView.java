f6b2c6aceb6ef306853dc232725745d5a208f81f
CMvvvt <452000761@qq.com>
2021-06-07 21:29:50 +0800

    P199 depth first search method added

--- src/P199_BTRightSIdeView.java
+++ src/P199_BTRightSIdeView.java
@@ -34,4 +34,49 @@ public class P199_BTRightSIdeView {
             return res;
         }
     }
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution2 {
+        public List<Integer> rightSideView(TreeNode root) {
+            List<Integer> list = new ArrayList<>();
+            Map<Integer, Integer> rightMostAtLevel = new HashMap<>();
+            Stack<TreeNode> treeStack = new Stack<>();
+            Stack<Integer> levelStack = new Stack<>();
+
+            int maxDepth = -1;
+            treeStack.push(root);
+            levelStack.push(0);
+            while(! treeStack.isEmpty()) {
+                TreeNode node = treeStack.pop();
+                int depth = levelStack.pop();
+                if(node != null) {
+                    maxDepth = Math.max(maxDepth, depth);
+                    if(!rightMostAtLevel.containsKey(depth)) {
+                        rightMostAtLevel.put(depth, node.val);
+                    }
+                    treeStack.push(node.left);
+                    treeStack.push(node.right);
+                    levelStack.push(depth+1);
+                    levelStack.push(depth+1);
+                }
+            }
+            for(int i = 0; i <= maxDepth; i++) {
+                list.add(rightMostAtLevel.get(i));
+            }
+            return list;
+        }
+    }
 }
