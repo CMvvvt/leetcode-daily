62ddbde747618e9eade033345a99c3fdd6889651
CMvvvt <452000761@qq.com>
2021-06-07 23:36:20 +0800

    P200 breadth first search added

--- src/P200_numbersOfIslands.java
+++ src/P200_numbersOfIslands.java
@@ -27,4 +27,44 @@ public class P200_numbersOfIslands {
             dfs(grid, i, j-1);
         }
     }
+
+    class Solution2 {
+        public int numIslands(char[][] grid) {
+            if(grid == null || grid.length == 0) return 0;
+            int m = grid.length, n = grid[0].length;
+            int count = 0;
+            for(int i = 0; i < m; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(grid[i][j] == '1') {
+                        count++;
+                        grid[i][j] = '0';
+                        Queue<Integer> queue = new LinkedList<>();
+                        queue.add(i * n + j);
+                        while(! queue.isEmpty()){
+                            int id = queue.remove();
+                            int row = id / n;
+                            int col = id % n;
+                            if(row-1 >= 0 && grid[row-1][col] == '1') {
+                                grid[row-1][col] = '0';
+                                queue.add((row-1)*n + col);
+                            }
+                            if(row+1 < m && grid[row+1][col] == '1') {
+                                grid[row+1][col] = '0';
+                                queue.add((row+1)*n + col);
+                            }
+                            if(col+1 < n && grid[row][col+1] == '1') {
+                                grid[row][col+1] = '0';
+                                queue.add((row)*n + col+1);
+                            }
+                            if(col-1 >= 0 && grid[row][col-1] == '1') {
+                                grid[row][col-1] = '0';
+                                queue.add((row)*n + col-1);
+                            }
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
 }
