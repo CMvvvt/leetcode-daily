1a76a6d378b85ae3335705e8417b4013e7622c38
CMvvvt <452000761@qq.com>
2021-06-13 11:30:31 +0800

    P201 new method added

--- src/P201_bitwiseANDofNumbersRange.java
+++ src/P201_bitwiseANDofNumbersRange.java
@@ -10,4 +10,18 @@ public class P201_bitwiseANDofNumbersRange {
             return left << shift;
         }
     }
+
+    /**
+     * Brian Kernighan algorithm added.
+     * n & (n-1) would remove the last '1' of the integer, utill integer right <= integer left, then we have pre-string
+     * in common and all filled 0s at the right of integer right.
+     */
+    class Solution2 {
+        public int rangeBitwiseAnd(int left, int right) {
+            while(left < right) {
+                right &= right-1;
+            }
+            return right;
+        }
+    }
 }
