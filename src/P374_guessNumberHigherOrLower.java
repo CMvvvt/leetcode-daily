280377c7e0439ed9e796f48b9566c1a34827a57a
CMvvvt <452000761@qq.com>
2021-06-14 09:40:23 +0800

    P374 added new version of binary search

--- src/P374_guessNumberHigherOrLower.java
+++ src/P374_guessNumberHigherOrLower.java
@@ -20,4 +20,28 @@ public class P374_guessNumberHigherOrLower {
             return helper(mid+1, high);
         }
     }
+
+    /**
+     * Forward declaration of guess API.
+     * @param  num   your guess
+     * @return 	     -1 if num is lower than the guess number
+     *			      1 if num is higher than the guess number
+     *               otherwise return 0
+     * int guess(int num);
+     */
+
+    public class Solution2 extends GuessGame {
+        public int guessNumber(int n) {
+            int low = 1, high = n;
+            while(low < high) {
+                int mid = low + (high - low)/2;
+                if(guess(mid) <= 0) {
+                    high = mid;
+                } else {
+                    low = mid+1;
+                }
+            }
+            return low;
+        }
+    }
 }
