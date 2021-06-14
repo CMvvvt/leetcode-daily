ccedf672a904deacff78b9279338b9f72e47fa98
CMvvvt <452000761@qq.com>
2021-06-14 18:31:33 +0800

    P207 added bfs method

--- src/P207_courseSchedules.java
+++ src/P207_courseSchedules.java
@@ -1,5 +1,5 @@
 public class P207_courseSchedules {
-    class Solution {
+    class Solution1 {
         List<List<Integer>> edges;
         int[] visited;
         boolean valid = true;
@@ -33,6 +33,43 @@ public class P207_courseSchedules {
                 }
             }
             visited[u] = 3;
+        }
+    }
+
+    /**
+     *  bfs method
+     */
+    class Solution2 {
+        List<List<Integer>> edges;
+        int[] indegs;
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            edges = new ArrayList<>();
+            indegs = new int[numCourses];
+            for(int i = 0; i < numCourses; i++) {
+                edges.add(new ArrayList(i));
+            }
+            for(int[] pre: prerequisites) {
+                edges.get(pre[1]).add(pre[0]);
+                indegs[pre[0]]++;
+            }
+            Queue<Integer> queue = new LinkedList<>();
+            for(int i = 0; i < numCourses; i++) {
+                if(indegs[i] == 0) {
+                    queue.offer(i);
+                }
+            }
+            int visited = 0;
+            while(! queue.isEmpty()) {
+                visited++;
+                int index = queue.poll();
+                for(int v: edges.get(index)) {
+                    indegs[v]--;
+                    if(indegs[v] == 0) {
+                        queue.offer(v);
+                    }
+                }
+            }
+            return visited == numCourses;
         }
     }
 }
