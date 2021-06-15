93af4df257db4b4cc41530befbc4cd61021255c5
CMvvvt <452000761@qq.com>
2021-06-15 10:51:02 +0800

    P852 added new method

--- src/P852_peakIndexInMountain.java
+++ src/P852_peakIndexInMountain.java
@@ -1,5 +1,5 @@
 public class P852_peakIndexInMountain {
-    class Solution {
+    class Solution1 {
         public int peakIndexInMountainArray(int[] arr) {
             int len = arr.length;
             int low = 0, high = len - 1;
@@ -15,6 +15,23 @@ public class P852_peakIndexInMountain {
                 }
             }
             return low;
+        }
+    }
+
+    class Solution2 {
+        public int peakIndexInMountainArray(int[] arr) {
+            int low = 0, high = arr.length-2;
+            int ans = -1;
+            while(low <= high) {
+                int mid = low + (high-low)/2;
+                if(arr[mid] > arr[mid+1]) {
+                    ans = mid;
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            return ans;
         }
     }
 }
