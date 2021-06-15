e54124288da82d3edd751f3cf662f1d9b5c828b4
CMvvvt <452000761@qq.com>
2021-06-15 15:46:13 +0800

    P189 added reverse method

--- src/P189_rotateArray.java
+++ src/P189_rotateArray.java
@@ -53,4 +53,20 @@ public class P189_rotateArray {
             return;
         }
     }
+    class Solution4 {
+        public void rotate(int[] nums, int k) {
+            reverse(nums, 0, nums.length-1);
+            reverse(nums, 0, k % nums.length - 1);
+            reverse(nums, k % nums.length, nums.length-1);
+        }
+        private void reverse(int[] nums, int left, int right) {
+            while(left < right) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
 }
