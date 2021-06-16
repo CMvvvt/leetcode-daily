a109f5034d810f3b16f6105e0b557426764d2540
CMvvvt <452000761@qq.com>
2021-06-16 17:34:19 +0800

    P234 reverse method added

--- src/P234_palindromeLinkedList.java
+++ src/P234_palindromeLinkedList.java
@@ -35,4 +35,52 @@ public class P234_palindromeLinkedList {
             return true;
         }
     }
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution2 {
+        public boolean isPalindrome(ListNode head) {
+            ListNode mid = getMid(head);
+            ListNode reverseHead = reverse(mid.next);
+            ListNode p1 = head, p2 = reverseHead;
+            boolean result = true;
+            while(result && p2 != null) {
+                if(p1.val != p2.val) {
+                    result = false;
+                    break;
+                }
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+            mid.next = reverse(reverseHead);
+            return result;
+        }
+        private ListNode getMid(ListNode head) {
+            ListNode slow = head, fast = head;
+            while(fast.next != null && fast.next.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
+        private ListNode reverse(ListNode head) {
+            if(head == null || head.next == null) return head;
+            ListNode prev = null;
+            ListNode curr = head;
+            while(curr != null) {
+                ListNode next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            return prev;
+        }
+    }
 }
