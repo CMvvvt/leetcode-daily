9a2d992e99fb5b3c3ea710fcd4399f598da82386
CMvvvt <452000761@qq.com>
2021-06-16 21:26:43 +0800

    P237 added fastest method

--- src/P237_deleteNodeInALinkedList.java
+++ src/P237_deleteNodeInALinkedList.java
@@ -19,4 +19,18 @@ public class P237_deleteNodeInALinkedList {
             }
         }
     }
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public void deleteNode(ListNode node) {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
 }
