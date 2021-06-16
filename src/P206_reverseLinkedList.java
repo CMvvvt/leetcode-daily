e84f92e5abe57ddece7891ef8f05480e945b832b
CMvvvt <452000761@qq.com>
2021-06-16 14:56:59 +0800

    P206 recursion method added

--- src/P206_reverseLinkedList.java
+++ src/P206_reverseLinkedList.java
@@ -22,4 +22,24 @@ public class P206_reverseLinkedList {
             return prev;
         }
     }
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution2 {
+        public ListNode reverseList(ListNode head) {
+            if(head == null || head.next == null) return head;
+            ListNode newHead = reverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+            return newHead;
+        }
+    }
 }
