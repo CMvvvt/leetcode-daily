0c09496e7fa70d78393d732944e015012873f9d1
CMvvvt <452000761@qq.com>
2021-06-16 11:24:44 +0800

    P486 added dynamic programming method

--- src/P486_predictTheWinner.java
+++ src/P486_predictTheWinner.java
@@ -10,4 +10,24 @@ public class P486_predictTheWinner {
             return turn > 0 ? Math.max(numEnd, numStart) : Math.min(numStart, numEnd);
         }
     }
+
+    /**
+     * dp method
+     *
+     */
+    class Solution2 {
+        public boolean PredictTheWinner(int[] nums) {
+            int n = nums.length;
+            int[][] dp = new int[n][n];
+            for(int i = 0; i < n; i++) {
+                dp[i][i] = nums[i];
+            }
+            for(int i = n - 2; i >= 0; i--) {
+                for(int j = i + 1; j < n; j++) {
+                    dp[i][j] = Math.max(nums[i] - dp[i+1][j], nums[j] - dp[i][j-1]);
+                }
+            }
+            return dp[0][n-1] >= 0;
+        }
+    }
 }
