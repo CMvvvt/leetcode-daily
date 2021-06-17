1f664dbab7e75ff0ab9cb177056e30f4ab07cc2a
CMvvvt <452000761@qq.com>
2021-06-17 19:20:41 +0800

    P82 recursion method added

--- src/P82_removeDupsFromSortedArrII.java
+++ src/P82_removeDupsFromSortedArrII.java
@@ -86,4 +86,29 @@ public class P82_removeDupsFromSortedArrII {
             return dummyHead.next;
         }
     }
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution4 {
+        public ListNode deleteDuplicates(ListNode head) {
+            if(head == null || head.next == null) return head;
+            if(head.val != head.next.val) {
+                head.next = deleteDuplicates(head.next);
+                return head;
+            } else {
+                ListNode start = head.next.next;
+                while(start != null && start.val == head.val) {
+                    start = start.next;
+                }
+                return deleteDuplicates(start);
+            }
+        }
+    }
 }
