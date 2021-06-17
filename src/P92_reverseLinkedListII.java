d7a24e94d6897cadca28682bc96f2623c260684b
CMvvvt <452000761@qq.com>
2021-06-17 12:10:37 +0800

    P92 added new method

--- src/P92_reverseLinkedListII.java
+++ src/P92_reverseLinkedListII.java
@@ -42,4 +42,51 @@ public class P92_reverseLinkedListII {
             return p;
         }
     }
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode reverseBetween(ListNode head, int left, int right) {
+            if(head.next == null) return head;
+            ListNode beforeStart = new ListNode();
+            beforeStart.next = head;
+            ListNode dummy = head;
+            int flag = left;
+            while(right > 1) {
+                left--;
+                right--;
+                if(left == 1) {
+                    beforeStart = dummy;
+                }
+                dummy = dummy.next;
+            }
+            ListNode start = beforeStart.next;
+            ListNode beforeEnd = dummy;
+            ListNode end = beforeEnd.next;
+            beforeEnd.next = null;
+            beforeStart.next = reverse(start);
+            start.next = end;
+            return flag == 1? beforeEnd: head;
+        }
+        private ListNode reverse(ListNode head) {
+            if(head == null) return head;
+            ListNode prev = null;
+            ListNode curr = head;
+            while(curr != null) {
+                ListNode next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            return prev;
+        }
+    }
 }
