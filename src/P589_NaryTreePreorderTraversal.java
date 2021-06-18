b0d6e65e5ad5998a50a52d880e4ead5e18d13cef
CMvvvt <452000761@qq.com>
2021-06-18 21:17:58 +0800

    P589 iteration method added

--- src/P589_NaryTreePreorderTraversal.java
+++ src/P589_NaryTreePreorderTraversal.java
@@ -35,4 +35,24 @@ class Node {
             return;
         }
     }
+
+
+    class Solution2 {
+        public List<Integer> preorder(Node root) {
+            List<Integer> list = new ArrayList<>();
+            if(root == null) return list;
+            Stack<Node> stack = new Stack<>();
+            stack.push(root);
+            while(! stack.isEmpty()) {
+                Node node = stack.pop();
+                list.add(node.val);
+                Collections.reverse(node.children);
+                for(Node child: node.children) {
+                    stack.push(child);
+                }
+            }
+            return list;
+        }
+    }
+
 }
