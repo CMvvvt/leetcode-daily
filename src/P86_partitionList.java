cc9e6507d1be9acb080f58b6561fb9dcb60ca11c
CMvvvt <452000761@qq.com>
2021-06-18 15:01:19 +0800

    P86 second time

--- src/P86_partitionList.java
+++ src/P86_partitionList.java
@@ -32,5 +32,36 @@ public class P86_partitionList {
         }
     }
 
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution2 {
+        public ListNode partition(ListNode head, int x) {
+            ListNode small = new ListNode(0);
+            ListNode smallHead = small;
+            ListNode large = new ListNode(0);
+            ListNode largeHead = large;
+            while(head != null) {
+                if(head.val < x) {
+                    smallHead.next = head;
+                    smallHead = smallHead.next;
+                } else {
+                    largeHead.next = head;
+                    largeHead = largeHead.next;
+                }
+                head = head.next;
+            }
+            largeHead.next = null;
+            smallHead.next = large.next;
+            return small.next;
+        }
+    }
 
 }
