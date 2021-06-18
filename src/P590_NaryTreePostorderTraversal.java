1e7128188c17283ec8d9df8e1cc01d70c3a1a14d
CMvvvt <452000761@qq.com>
2021-06-18 22:32:22 +0800

    P590 added iteration approach

--- src/P590_NaryTreePostorderTraversal.java
+++ src/P590_NaryTreePostorderTraversal.java
@@ -38,4 +38,28 @@ class Node {
             return;
         }
     }
+
+    /**
+     * ITERATION APPROACH
+     */
+    class Solution {
+        public List<Integer> postorder(Node root) {
+            List<Integer> res = new ArrayList<>();
+            if(root == null) return res;
+            Stack<Node> stack1 = new Stack<>();
+            Stack<Node> stack2 = new Stack<>();
+            stack1.push(root);
+            while(!stack1.isEmpty()) {
+                Node node = stack1.pop();
+                stack2.push(node);
+                for(Node child: node.children) {
+                    stack1.push(child);
+                }
+            }
+            while(!stack2.isEmpty()) {
+                res.add(stack2.pop().val);
+            }
+            return res;
+        }
+    }
 }
