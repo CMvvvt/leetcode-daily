bed96fcb8826897e34cf656d25a07cf5f0f4b90e
CMvvvt <452000761@qq.com>
2021-06-18 11:44:14 +0800

    P148 added merge sort methos

--- src/P148_sortList.java
+++ src/P148_sortList.java
@@ -34,4 +34,46 @@ public class P148_sortList {
             }
         }
     }
+
+    /**
+     * merge sort method added.
+     * form top to bottom
+     */
+    class Solution2 {
+        public ListNode sortList(ListNode head) {
+            if(head == null || head.next == null) return head;
+            ListNode mid = getMid(head);
+            ListNode l1 = head;
+            ListNode l2 = mid.next;
+            mid.next = null;
+            l1 = sortList(l1);
+            l2 = sortList(l2);
+            return mergeList(l1, l2);
+        }
+        private ListNode getMid(ListNode head) {
+            if(head == null || head.next == null) return head;
+            ListNode slow = head, fast = head;
+            while(fast.next != null && fast.next.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+        private ListNode mergeList(ListNode l1, ListNode l2) {
+            ListNode dummyHead = new ListNode(-1);
+            ListNode prev = dummyHead;
+            while(l1 != null && l2 != null) {
+                if(l1.val <= l2.val) {
+                    prev.next = l1;
+                    l1 = l1.next;
+                } else {
+                    prev.next = l2;
+                    l2 = l2.next;
+                }
+                prev = prev.next;
+            }
+            prev.next = l1 == null ? l2: l1;
+            return dummyHead.next;
+        }
+    }
 }
