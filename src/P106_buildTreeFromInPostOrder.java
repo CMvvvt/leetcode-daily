4652bbe2dfb26d185e866ac081013255668a1b05
CMvvvt <452000761@qq.com>
2021-06-20 17:34:24 +0800

    P106 added second times

--- src/P106_buildTreeFromInPostOrder.java
+++ src/P106_buildTreeFromInPostOrder.java
@@ -40,4 +40,41 @@ public class P106_buildTreeFromInPostOrder {
             return root;
         }
     }
+
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution2 {
+        private Map<Integer, Integer> index = new HashMap<>();
+        public TreeNode buildTree(int[] inorder, int[] postorder) {
+            int length = inorder.length;
+            for(int i = 0; i < length; i++) {
+                index.put(inorder[i], i);
+            }
+            return build(inorder, postorder, 0, length-1, 0, length-1);
+        }
+
+        private TreeNode build(int[] inorder, int[] postorder, int post_l, int post_r, int in_l, int in_r) {
+            if(in_l > in_r) return null;
+            TreeNode root = new TreeNode(postorder[post_r]);
+            int root_index = index.get(postorder[post_r]);
+            int size = in_r - root_index;
+            root.right = build(inorder, postorder, post_r - size, post_r-1, root_index +1, in_r);
+            root.left = build(inorder, postorder, post_l, post_r -size -1, in_l, root_index-1);
+            return root;
+        }
+    }
 }
