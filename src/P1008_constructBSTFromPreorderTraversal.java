f712574251295af870b7c02dba13723694051360
CMvvvt <452000761@qq.com>
2021-06-21 16:04:13 +0800

    P1008 added new method

--- src/P1008_constructBSTFromPreorderTraversal.java
+++ src/P1008_constructBSTFromPreorderTraversal.java
@@ -41,4 +41,47 @@ public class P1008_constructBSTFromPreorderTraversal {
             return root;
         }
     }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        public TreeNode bstFromPreorder(int[] preorder) {
+            int len = preorder.length;
+            if(len == 0) {
+                return null;
+            }
+            return dfs(preorder, 0, len-1);
+        }
+
+        private TreeNode dfs(int[] preorder, int left, int right) {
+            if(left > right) return null;
+            TreeNode node = new TreeNode(preorder[left]);
+            if(left == right) return node;
+            int l = left, r = right;
+            while(l < r) {
+                int mid = l + (r-l+1) / 2;
+                if(preorder[mid] < preorder[left]) {
+                    l = mid;
+                } else {
+                    r = mid - 1;
+                }
+            }
+            node.left = dfs(preorder, left+1, l);
+            node.right = dfs(preorder, l+1, right);
+            return node;
+        }
+    }
 }
