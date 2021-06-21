ddaf0c4f730603da6413c70c2d5d85a7955daea3
CMvvvt <452000761@qq.com>
2021-06-21 11:40:41 +0800

    P1600 added

---
+++ src/P1600_throneInheritance.java
@@ -0,0 +1,46 @@
+public class P1600_throneInheritance {
+    class ThroneInheritance {
+        Map<String, List<String>> edges;
+        Set<String> dead;
+        String king;
+        public ThroneInheritance(String kingName) {
+            edges = new HashMap<>();
+            dead = new HashSet<>();
+            king = kingName;
+        }
+
+        public void birth(String parentName, String childName) {
+            List<String> children = edges.getOrDefault(parentName, new ArrayList<String>());
+            children.add(childName);
+            edges.put(parentName, children);
+        }
+
+        public void death(String name) {
+            dead.add(name);
+        }
+
+        public List<String> getInheritanceOrder() {
+            List<String> ans = new ArrayList<>();
+            preorder(ans, king);
+            return ans;
+        }
+
+        private void preorder(List<String> ans, String name) {
+            if(!dead.contains(name)) {
+                ans.add(name);
+            }
+            List<String> children = edges.getOrDefault(name, new ArrayList<>());
+            for(String childName: children) {
+                preorder(ans, childName);
+            }
+        }
+    }
+
+/**
+ * Your ThroneInheritance object will be instantiated and called as such:
+ * ThroneInheritance obj = new ThroneInheritance(kingName);
+ * obj.birth(parentName,childName);
+ * obj.death(name);
+ * List<String> param_3 = obj.getInheritanceOrder();
+ */
+}
