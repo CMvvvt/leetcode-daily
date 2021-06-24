1ff530b49685f45b7b35a57444c3b53adcd96077
CMvvvt <452000761@qq.com>
2021-06-24 16:07:58 +0800

    P100 added mnew method

--- src/P100_sameTree.java
+++ src/P100_sameTree.java
@@ -22,4 +22,13 @@ public class P100_sameTree {
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
     }
+
+    class Solution2 {
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if(p == null && q == null) return true;
+            if(p == null || q == null) return false;
+            if(p.val != q.val) return false;
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
 }
