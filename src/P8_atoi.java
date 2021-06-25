1f313ce587b36d4462f6f315a291f5ccedb1904c
CMvvvt <452000761@qq.com>
2021-06-25 14:40:12 +0800

    P8 added new method

--- src/P8_atoi.java
+++ src/P8_atoi.java
@@ -44,4 +44,38 @@ public class P8_atoi {
         String s = "48";
         assertEquals(48,myAtoi(s));
     }
+
+    class Solution {
+        public int myAtoi(String s) {
+            int index = 0;
+            int sign = 1;
+            int total = 0;
+            // 1. Empty String
+            if(s.length() == 0) return 0;
+
+            // 2. Remove Spaces
+            while(index < s.length() && s.charAt(index) == ' ') {
+                index++;
+            }
+
+            // 3. Handle Signs
+            if(index < s.length() && (s.charAt(index) == '+' || s.charAt(index) == '-')) {
+                sign = s.charAt(index) == '+' ? 1: -1;
+                index++;
+            }
+
+            // 4. Convert Number and Avoid Overflows
+            while(index < s.length()) {
+                int digit = s.charAt(index) - '0';
+                if(digit < 0 || digit > 9) break;
+
+                // check total would be overflow after 10 times and add digit
+                if(Integer.MAX_VALUE/10 < total|| (Integer.MAX_VALUE/10 == total && Integer.MAX_VALUE%10 < digit))
+                    return sign == 1? Integer.MAX_VALUE: Integer.MIN_VALUE;
+                total = total * 10 + digit;
+                index++;
+            }
+            return sign*total;
+        }
+    }
 }
