659b24fbe03bd495accc869ae8ce90a546f1b555
CMvvvt <452000761@qq.com>
2021-06-25 20:05:33 +0800

    P11 added double pointer method

--- src/P11_containerWithMostWater.java
+++ src/P11_containerWithMostWater.java
@@ -13,4 +13,22 @@ public class P11_containerWithMostWater {
             return maxArea;
         }
     }
+
+    /**
+     * double pointer method
+     */
+    class Solution2 {
+        public int maxArea(int[] height) {
+            int l = 0, r = height.length - 1, maxArea = 0;
+            while(l < r) {
+                maxArea = Math.max(maxArea, (r-l)*Math.min(height[l], height[r]));
+                if(height[l] <= height[r]) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+            return maxArea;
+        }
+    }
 }
