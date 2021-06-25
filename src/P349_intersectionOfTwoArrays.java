fae309a90d40a6848f5bd225171d211b849ca1b9
CMvvvt <452000761@qq.com>
2021-06-25 13:39:09 +0800

    P349 added double pointer method

--- src/P349_intersectionOfTwoArrays.java
+++ src/P349_intersectionOfTwoArrays.java
@@ -2,7 +2,7 @@ public class P349_intersectionOfTwoArrays {
     /**
      *  set method
      */
-    class Solution {
+    class Solution1 {
         public int[] intersection(int[] nums1, int[] nums2) {
             Set<Integer> set1 = new HashSet<>();
             Set<Integer> set2 = new HashSet<>();
@@ -26,6 +26,36 @@ public class P349_intersectionOfTwoArrays {
             int[] res = new int[interSet.size()];
             int index = 0;
             for(int num: interSet) {
+                res[index++] = num;
+            }
+            return res;
+        }
+    }
+
+
+    /**
+     * double pointer method added
+     *
+     */
+    class Solution2 {
+        public int[] intersection(int[] nums1, int[] nums2) {
+            Arrays.sort(nums1);
+            Arrays.sort(nums2);
+            Set<Integer> set = new HashSet<>();
+            int i = 0, j = 0;
+            while(i < nums1.length && j < nums2.length) {
+                if(nums1[i] == nums2[j]) {
+                    set.add(nums1[i]);
+                }
+                if(nums1[i] < nums2[j]) {
+                    i++;
+                } else {
+                    j++;
+                }
+            }
+            int[] res = new int[set.size()];
+            int index = 0;
+            for(int num: set) {
                 res[index++] = num;
             }
             return res;
