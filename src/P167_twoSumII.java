45188ccef929b3b7703c4259f0c42ab8ffd8edb1
CMvvvt <452000761@qq.com>
2021-06-26 11:50:25 +0800

    P167 added new method

--- src/P167_twoSumII.java
+++ src/P167_twoSumII.java
@@ -65,4 +65,23 @@ public class P167_twoSumII {
             return new int[]{-1, -1};
         }
     }
+
+    class Solution5 {
+        public int[] twoSum(int[] numbers, int target) {
+            int left = 0;
+            int right = numbers.length - 1;
+            while(left < right) {
+                if(numbers[left] + numbers[right] == target) {
+                    return new int[] {left+1, right+1};
+                }
+                if(numbers[left] + numbers[right] > target) {
+                    right--;
+                    continue;
+                }
+                left++;
+                continue;
+            }
+            return new int[]{-1, -1};
+        }
+    }
 }
