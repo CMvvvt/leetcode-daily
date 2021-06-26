7263cb060a621e16640699d30259a5efbf2bad91
CMvvvt <452000761@qq.com>
2021-06-26 22:17:54 +0800

    P48 added new method

--- src/P48_rotateImage.java
+++ src/P48_rotateImage.java
@@ -18,4 +18,36 @@ public class P48_rotateImage {
             }
         }
     }
+
+    /**
+     * in-place method added
+     */
+    class Solution2 {
+        public void rotate(int[][] matrix) {
+            transpose(matrix);
+            reflect(matrix);
+        }
+
+        private void transpose(int[][] matrix) {
+            int n = matrix.length;
+            for(int i = 0; i < n; i++) {
+                for(int j = i + 1; j < n; j++) {
+                    int temp = matrix[j][i];
+                    matrix[j][i] = matrix[i][j];
+                    matrix[i][j] = temp;
+                }
+            }
+        }
+
+        private void reflect(int[][] matrix) {
+            int n = matrix.length;
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n / 2; j++) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[i][n-j-1];
+                    matrix[i][n-j-1] = temp;
+                }
+            }
+        }
+    }
 }
