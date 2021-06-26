dea4b7d6f3b2bf6856aa48edd9b7f2dca6880432
CMvvvt <452000761@qq.com>
2021-06-26 23:04:39 +0800

    P49 added bad solution

--- src/P49_groupAnagrams.java
+++ src/P49_groupAnagrams.java
@@ -13,4 +13,47 @@ public class P49_groupAnagrams {
         }
         return new ArrayList<List<String>>(map.values());
     }
+
+    /**
+     * extremely bad approach
+     */
+    class Solution2 {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            List<List<String>> res = new ArrayList<>();
+            int n = strs.length;
+            if(n == 0) return res;
+            boolean[] visited = new boolean[n];
+
+            for(int i = 0; i < n; i++) {
+                if(visited[i]) continue;
+                visited[i] = true;
+                List<String> list = new ArrayList<>();
+                list.add(strs[i]);
+
+                int length = strs[i].length();
+                for(int j = i + 1; j < n; j++) {
+                    if(strs[j].length() != length || visited[j]) {
+                        continue;
+                    }
+                    if(isAnagram(strs[i], strs[j])) {
+                        visited[j] = true;
+                        list.add(strs[j]);
+                    }
+                }
+                res.add(list);
+            }
+            return res;
+        }
+
+        private boolean isAnagram(String str1, String str2) {
+            char[] ch1 = str1.toCharArray();
+            char[] ch2 = str2.toCharArray();
+            Arrays.sort(ch1);
+            Arrays.sort(ch2);
+            String s1 = String.valueOf(ch1);
+            String s2 = String.valueOf(ch2);
+            return s1.equals(s2);
+
+        }
+    }
 }
