43ad10a95676baf55e1ca288363c88901fb18914
CMvvvt <452000761@qq.com>
2021-06-28 22:53:32 +0800

    P930 add method

---
+++ src/P930_binarySubarraysWithSum.java
@@ -0,0 +1,17 @@
+public class P930_binarySubarraysWithSum {class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, pre = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        for(int num: nums) {
+            pre += num;
+            if(map.containsKey(pre-k)) {
+                count += map.get(pre-k);
+            }
+            map.put(pre, map.getOrDefault(pre, 0) + 1);
+        }
+        return count;
+    }
+}
+
+}
