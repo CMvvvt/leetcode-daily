95d8d8d738d5bed68e58887dc15edbd1962cd4d2
CMvvvt <452000761@qq.com>
2021-06-28 22:30:00 +0800

    P560 added new approach

--- src/P560_subarraySumEqualsK.java
+++ src/P560_subarraySumEqualsK.java
@@ -14,4 +14,23 @@ public class P560_subarraySumEqualsK {
             return count;
         }
     }
+
+    /**
+     *  前缀和 approach
+     */
+    class Solution {
+        public int subarraySum(int[] nums, int k) {
+            int count = 0, pre = 0;
+            HashMap<Integer, Integer> map = new HashMap<>();
+            map.put(0,1);
+            for(int num: nums) {
+                pre += num;
+                if(map.containsKey(pre-k)) {
+                    count += map.get(pre-k);
+                }
+                map.put(pre, map.getOrDefault(pre, 0) + 1);
+            }
+            return count;
+        }
+    }
 }
