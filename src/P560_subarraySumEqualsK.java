e0fccf39f639d93c0828a923f1040435b2acec05
CMvvvt <452000761@qq.com>
2021-06-29 15:14:17 +0800

    P560 added new method

--- src/P560_subarraySumEqualsK.java
+++ src/P560_subarraySumEqualsK.java
@@ -33,4 +33,27 @@ public class P560_subarraySumEqualsK {
             return count;
         }
     }
+
+    /**
+     * cumulative sum method
+     */
+    class Solution {
+        public int subarraySum(int[] nums, int k) {
+            int n = nums.length;
+            int count = 0;
+            int[] sum = new int[n+1];
+            sum[0] = 0;
+            for(int i = 1; i <= n; i++) {
+                sum[i] = sum[i-1] + nums[i-1];
+            }
+            for(int i = 0; i < n; i++) {
+                for(int j = i+1; j <= n; j++) {
+                    if(sum[j] - sum[i] == k) {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
 }
