37332f5cd44a7e184360707e3b6beabfdb08278e
CMvvvt <452000761@qq.com>
2021-07-02 11:37:23 +0800

    P387 added new HashMap method

--- src/P387_firstUniqueCharInAString.java
+++ src/P387_firstUniqueCharInAString.java
@@ -13,4 +13,30 @@ public class P387_firstUniqueCharInAString {
             return -1;
         }
     }
+    /**
+     * HashMap stores is or not
+     */
+    class Solution {
+        public int firstUniqChar(String s) {
+            int n = s.length();
+            Map<Character, Integer> map = new HashMap<>();
+            for(int i = 0; i < n; i++) {
+                char ch = s.charAt(i);
+                if(map.containsKey(ch)) {
+                    map.put(ch, -1);
+                } else {
+                    map.put(ch, i);
+                }
+            }
+            int first = n;
+            for(Map.Entry<Character, Integer> entry: map.entrySet()) {
+                int pos = entry.getValue();
+                if(pos != -1 && pos < first) {
+                    first = pos;
+                }
+            }
+            return first == n? -1: first;
+        }
+
+    }
 }
