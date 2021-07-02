882caa3f6caa99098c877696dbeed88fd4049656
CMvvvt <452000761@qq.com>
2021-07-02 11:51:04 +0800

    P20 added new method

--- src/P20_isValid.java
+++ src/P20_isValid.java
@@ -61,4 +61,33 @@ public class P20_isValid {
         String s = "{[()]}";
         assertEquals(true,isValid(s));
     }
+
+
+    class Solution2 {
+        public boolean isValid(String s) {
+            int n = s.length();
+            if(n % 2 != 0) return false;
+            Stack<Character> stack = new Stack();
+            for(int i = 0; i < n; i++) {
+                char ch = s.charAt(i);
+                if(isOpen(ch)) {
+                    stack.push(ch);
+                    continue;
+                }
+                if(stack.isEmpty()) return false;
+                if(!isMatch(ch, stack.pop())) return false;
+            }
+            return stack.isEmpty();
+        }
+
+        private boolean isOpen(Character ch) {
+            return ch == '(' || ch == '{' || ch == '[';
+        }
+
+        private boolean isMatch(Character ch1, Character ch2) {
+            if(ch1 == ')') return ch2 =='(';
+            if(ch1 == ']') return ch2 == '[';
+            return ch2 == '{';
+        }
+    }
 }
