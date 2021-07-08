dc91ef0fc5dfcb0694c5a748060a2d47215f3ae7
CMvvvt <452000761@qq.com>
2021-07-08 19:19:00 +0800

    P930 added

--- src/P930_binarySubarraysWithSum.java
+++ src/P930_binarySubarraysWithSum.java
@@ -12,6 +12,23 @@ public class P930_binarySubarraysWithSum {class Solution {
         }
         return count;
     }
+
+    class Solution2 {
+        public int numSubarraysWithSum(int[] nums, int goal) {
+            int sum = 0;
+            Map<Integer, Integer> map = new HashMap<>();
+            map.put(0, 1);
+            int res = 0;
+            for(int num: nums) {
+                sum += num;
+                if(map.containsKey(sum - goal)) {
+                    res += map.get(sum-goal);
+                }
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+            return res;
+        }
+    }
 }
 
 }
