c9d5718d06a5cf29bb22674e8e65a304003d46de
CMvvvt <452000761@qq.com>
2021-07-14 20:35:43 +0800

    P146 added selfmade method

--- src/P146_LRUCache.java
+++ src/P146_LRUCache.java
@@ -70,6 +70,105 @@ public class P146_LRUCache {
         }
     }
 
+
+    /**
+     * method 2
+     */
+    class Node {
+        public int key, value;
+        public Node next, prev;
+        public Node(int k, int v) {
+            this.key = k;
+            this.value = v;
+        }
+    }
+    class DoubleLinkedList {
+
+        private Node head, tail;
+        private int size;
+
+        public void addFirst(Node node) {
+            if (head == null) {
+                head = tail = node;
+            } else {
+                Node n = head;
+                n.prev = node;
+                node.next = n;
+                head = node;
+            }
+            size++;
+        }
+
+        public void remove(Node node) {
+            if (head == node && tail == node) {
+                head = null;
+                tail = null;
+            } else if (tail == node) {
+                node.prev.next = null;
+                tail = node.prev;
+            } else if (head == node) {
+                node.next.prev = null;
+                head = node.next;
+            } else {
+                node.prev.next = node.next;
+                node.next.prev = node.prev;
+            }
+            size--;
+        }
+
+        public Node removeLast() {
+            Node node = tail;
+            remove(tail);
+            return node;
+        }
+
+        public int size() {
+            return size;
+        }
+    }
+
+
+    class LRUCache {
+        private HashMap<Integer, Node> map;
+        private DoubleLinkedList cache;
+        private int cap;
+
+        public LRUCache(int capacity) {
+            this.cap = capacity;
+            map = new HashMap<>();
+            cache = new DoubleLinkedList();
+        }
+
+        public int get(int key) {
+            if(!map.containsKey(key)) return -1;
+            int val = map.get(key).value;
+            put(key, val);
+            return val;
+        }
+
+        public void put(int key, int value) {
+            Node node = new Node(key, value);
+            if(map.containsKey(key)) {
+                cache.remove(map.get(key));
+                cache.addFirst(node);
+                map.put(key, node);
+            } else {
+                if(cap == cache.size()) {
+                    Node removeNode = cache.removeLast();
+                    map.remove(removeNode.key);
+                }
+                cache.addFirst(node);
+                map.put(key, node);
+            }
+        }
+    }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache obj = new LRUCache(capacity);
