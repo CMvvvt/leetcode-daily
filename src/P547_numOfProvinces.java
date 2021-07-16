bfa6d66f4eb8e83783cc08b9a8f3a4abfe830a68
CMvvvt <452000761@qq.com>
2021-07-16 12:30:33 +0800

    P547 added unionFind method

--- src/P547_numOfProvinces.java
+++ src/P547_numOfProvinces.java
@@ -52,4 +52,76 @@ public class P547_numOfProvinces {
             return provinces;
         }
     }
+
+
+    /**
+     * Union Find method added
+     */
+    class UnionFind {
+        private Map<Integer, Integer> father;
+        private int setsNum;
+        public UnionFind() {
+            father = new HashMap<>();
+            setsNum = 0;
+        }
+
+        public void add(int x) {
+            if(! father.containsKey(x)) {
+                father.put(x, null);
+                setsNum++;
+            }
+
+        }
+
+        public int find(int x) {
+            int root = x;
+
+            while(father.get(root) != null) {
+                root = father.get(root);
+            }
+
+            while(x != root) {
+                int originFather = father.get(x);
+                father.put(x, root);
+                x = originFather;
+            }
+
+            return root;
+        }
+
+        public void merge(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+
+            if(rootX != rootY) {
+                father.put(rootX, rootY);
+                setsNum--;
+            }
+
+        }
+
+        public boolean isConnected(int x, int y) {
+            return find(x) == find(y);
+        }
+
+        public int size() {
+            return setsNum;
+        }
+    }
+
+    class Solution {
+        public int findCircleNum(int[][] isConnected) {
+            int numbers = isConnected.length;
+            UnionFind union = new UnionFind();
+            for(int i = 0; i < numbers; i++) {
+                union.add(i);
+                for(int j = 0; j < i; j++) {
+                    if(isConnected[i][j] == 1) {
+                        union.merge(i, j);
+                    }
+                }
+            }
+            return union.size();
+        }
+    }
 }
