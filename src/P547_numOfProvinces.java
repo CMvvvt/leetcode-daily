d4016cb7cc366660f5be95373a889b1b74fda911
CMvvvt <452000761@qq.com>
2021-07-16 11:06:42 +0800

    P547 added BFS method

--- src/P547_numOfProvinces.java
+++ src/P547_numOfProvinces.java
@@ -23,4 +23,33 @@ public class P547_numOfProvinces {
             return;
         }
     }
+
+
+    /**
+     *  breadth first search method added
+     */
+    class Solution {
+        public int findCircleNum(int[][] isConnected) {
+            int numbers = isConnected.length;
+            int provinces = 0;
+            boolean[] visited = new boolean[numbers];
+            Queue<Integer> queue = new LinkedList<>();
+            for(int i = 0; i < numbers; i++) {
+                if(! visited[i]) {
+                    queue.offer(i);
+                    while(! queue.isEmpty()) {
+                        int j = queue.poll();
+                        visited[j] = true;
+                        for(int k = 0; k < numbers; k++) {
+                            if(isConnected[j][k] == 1 && ! visited[k]) {
+                                queue.offer(k);
+                            }
+                        }
+                    }
+                    provinces++;
+                }
+            }
+            return provinces;
+        }
+    }
 }
