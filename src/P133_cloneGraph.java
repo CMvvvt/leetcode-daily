db5849164d06a6a83d7bb37da72a6e08a9ee74e5
CMvvvt <452000761@qq.com>
2021-07-18 20:17:42 +0800

    P133 added bfs method

--- src/P133_cloneGraph.java
+++ src/P133_cloneGraph.java
@@ -38,4 +38,51 @@ class Node {
             return cloneNode;
         }
     }
+
+
+    /**
+     *  bfs method
+     */
+    /*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+    class Solution {
+        public Node cloneGraph(Node node) {
+            if(node == null) return node;
+            Map<Node, Node> visited = new HashMap<>();
+
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(node);
+            visited.put(node, new Node(node.val, new ArrayList<>()));
+            while(! queue.isEmpty()) {
+                Node n = queue.poll();
+                for(Node neighbor: n.neighbors) {
+                    if(! visited.containsKey(neighbor)) {
+                        visited.put(neighbor, new Node(neighbor.val, new ArrayList<>()));
+                        queue.add(neighbor);
+                    }
+                    visited.get(n).neighbors.add(visited.get(neighbor));
+                }
+            }
+            return visited.get(node);
+
+        }
+    }
 }
