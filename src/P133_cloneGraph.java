8615fd66b52161b1d581c66064f69ea222a81404
CMvvvt <452000761@qq.com>
2021-07-18 19:55:22 +0800

    P133 added

---
+++ src/P133_cloneGraph.java
@@ -0,0 +1,41 @@
+public class P133_cloneGraph {
+    /*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+    class Solution {
+        Map<Node, Node> map = new HashMap<>();
+        public Node cloneGraph(Node node) {
+            if(node == null) return node;
+
+            if(map.containsKey(node)) {
+                return map.get(node);
+            }
+
+            Node cloneNode = new Node(node.val, new ArrayList<>());
+            map.put(node, cloneNode);
+
+            for(Node neighbor: node.neighbors) {
+                cloneNode.neighbors.add(cloneGraph(neighbor));
+            }
+
+            return cloneNode;
+        }
+    }
+}
